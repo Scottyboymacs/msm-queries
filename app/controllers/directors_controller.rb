f1cae6ce44821b58762973b185010e2b43c433cb
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-05-04 23:44:12 +0000

    WHat is going on ??

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,20 +6,24 @@ class DirectorsController < ApplicationController
   end
 
   def eldest
-    @director_list = Director.all
+    @the_oldest = Director.all.order({:dob => :asc}).first
+    @oldest_id = @the_oldest[:id]
+    @oldest_name = @the_oldest[:name]
+    @oldest_dob = @the_oldest[:dob]
     render({ :template => "templates/director_templates/eldest.html.erb"})
   end
 
   def display
-    @director_id = params.fetch("director_id")
-    @director_list = Director.all
-    this_director = @director_list.where( {:id => @director_id })
+    if params.fetch("director_id").class == "eldest"
+      @director_id = params.fetch("director_id")
+      @director_list = Director.all
+      this_director = @director_list.where( {:id => @director_id })
  
       @name = this_director[0][:name]
       @dob = this_director[0].fetch(:dob)
       @image = "http://robohash.org/#{@name}"
       @bio = this_director[0].fetch(:bio)
-
+    end  
     render({ :template => "templates/director_templates/dynamic.html.erb"})
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
-  get("/directors/:director_id", { :controller => "directors", :action => "display"})
   get("/directors/eldest", { :controller => "directors", :action => "eldest"})
+  get("/directors/:director_id", { :controller => "directors", :action => "display"})
 end

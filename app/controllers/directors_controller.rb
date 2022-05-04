35f2ed8bca7a6fcb368da77556e061cb1799ce06
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-05-04 22:58:59 +0000

    Dynamic Director Page Complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,16 +5,21 @@ class DirectorsController < ApplicationController
     render({ :template => "templates/director_templates/index.html.erb"})
   end
 
+  def eldest
+    @director_list = Director.all
+    render({ :template => "templates/director_templates/eldest.html.erb"})
+  end
 
   def display
-    @director_id = params.get("director_id")
+    @director_id = params.fetch("director_id")
     @director_list = Director.all
-    @this_director = @director_list[@director_id]
-    @name = 
-    @dob = 
-    @image = 
-    @bio = 
-     
+    this_director = @director_list.where( {:id => @director_id })
+ 
+      @name = this_director[0][:name]
+      @dob = this_director[0].fetch(:dob)
+      @image = "http://robohash.org/#{@name}"
+      @bio = this_director[0].fetch(:bio)
+
     render({ :template => "templates/director_templates/dynamic.html.erb"})
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:director_id", { :controller => "directors", :action => "display"})
-
+  get("/directors/eldest", { :controller => "directors", :action => "eldest"})
 end

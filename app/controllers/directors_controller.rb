f0d791b5b36880d90da696124851b21d84fc6a2a
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-05-04 21:53:34 +0000

    Another Commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,17 +1,20 @@
 class DirectorsController < ApplicationController
 
   def index
+    @director_list = Director.all
     render({ :template => "templates/director_templates/index.html.erb"})
   end
 
 
   def display
-    @director = params.get("director_name")
+    @director_id = params.get("director_id")
+    @director_list = Director.all
+    @this_director = @director_list[@director_id]
     @name = 
     @dob = 
     @image = 
     @bio = 
-    @director_id = 
+     
     render({ :template => "templates/director_templates/dynamic.html.erb"})
   end
 
